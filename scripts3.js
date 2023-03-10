1b8257b0b2cd1bc31753abf9a5a818853f65d476
fernfrn <piyaratt.sittiyos@gmail.com>
2023-03-10 13:40:40 +0700

    test

--- scripts3.js
+++ scripts3.js
@@ -2,3 +2,16 @@ console.log('Hello world');
 
 let email = 'piyaratt.sittiyos@gmail.com';
 console.log(email)
+
+let firstName = 'Piyaratt';
+let lastName = 'Sittiyos';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let info = `My name: ${fullName} and I am ${firstName}`;
+console.log(info);
+
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
