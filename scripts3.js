ca61acc49180ff51080bfff1f3ef40b461b6a91e
fernfrn <piyaratt.sittiyos@gmail.com>
2023-03-11 12:14:29 +0700

    do homework

--- scripts3.js
+++ scripts3.js
@@ -23,4 +23,15 @@ let x = 123;
 //x = "Me";
 console.log(typeof x);
 
+//homework JS-2
+let birthYear = 1991;
+let futureYear = 2030;
+ageCalculator = futureYear-birthYear;
+console.log(ageCalculator);
 
+let person = {
+    Name: 'John',
+    Location: 'Canada',
+    Hobby: 'Hiking'
+}
+console.log(`Hi, My name is ${person.Name}, I live in ${person.Location} and enjoy ${person.Hobby}`);
